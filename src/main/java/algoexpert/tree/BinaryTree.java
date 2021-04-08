24a6d0c9958e21b555114aba664fe38272d6da9c
simiaolin <simiao.lin@student.kuleuven.be>
2021-04-08 22:13:44 +0200

    [add] two on tree.

---
+++ src/main/java/algoexpert/tree/BinaryTree.java
@@ -0,0 +1,18 @@
+package algoexpert.tree;
+
+public class BinaryTree {
+    int value;
+    BinaryTree left;
+    BinaryTree right;
+
+     BinaryTree(int value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+     BinaryTree(int value, BinaryTree left, BinaryTree right) {
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
+}
