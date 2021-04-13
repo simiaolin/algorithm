d6df6862698f651802ea899ef4d37f4755e42fab
simiaolin <simiao.lin@student.kuleuven.be>
2021-04-13 22:18:24 +0200

    [add] add 3 of LinkedList

---
+++ src/main/java/algoexpert/LinkedList/LinkedList.java
@@ -0,0 +1,26 @@
+package algoexpert.LinkedList;
+
+public class LinkedList {
+    public int value;
+    public LinkedList next;
+
+    public LinkedList(int value) {
+        this.value = value;
+        this.next = null;
+    }
+
+    public LinkedList(int value, LinkedList next) {
+        this.value = value;
+        this.next = next;
+    }
+    
+    public static LinkedList getLinkedListFromArray(int[] arr) {
+        LinkedList right = new LinkedList(arr[arr.length - 1]);
+        LinkedList left = null;
+        for (int i = arr.length - 2; i >=0 ;i--) {
+            left = new LinkedList(arr[i], right);
+            right = left;
+        }
+        return right;
+    }
+}
---
+++ src/main/java/algoexpert/LinkedList/RemoveKthNodeFromEnd.java
@@ -0,0 +1,34 @@
+package algoexpert.LinkedList;
+
+//是一道很好的理解边界的问题
+public class RemoveKthNodeFromEnd {
+    public static void removeKthNodeFromEnd(LinkedList head, int k) {
+        // Write your code here.
+        LinkedList right = head;
+        LinkedList left = head;
+        while (right != null && k > 0) {
+            right = right.next;
+            k--;
+        }
+        if (k > 0) {           //欲求不满，超出边界了
+            return;
+        }
+        if (right == null) { // to remove the head
+            head.value = head.next.value;
+            head.next = head.next.next;
+        } else {
+            while (right.next != null) {
+                left = left.next;
+                right = right.next;
+            }
+            left.next = left.next.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{0,1,2,3,4,5, 6,7,8,9};
+        LinkedList right = LinkedList.getLinkedListFromArray(arr);
+        removeKthNodeFromEnd(right, 4);
+        System.out.println("h");
+    }
+}
